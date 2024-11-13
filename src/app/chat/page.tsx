e3bbd7b5ff9290476401d46e9355dfed15b066bd
Salomon <salomond.c@hotmail.com>
2024-11-12 21:14:30 -0300

    Temp redirect from new chat to saved chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { ChatPlaceholder } from "@/components";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Chat() {
+	const router = useRouter();
+	useEffect(() => {
+		router.push("/chat/1");
+	}, []);
+
 	return <ChatPlaceholder />;
 }
