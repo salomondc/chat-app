c3e3a3dd28fc496e6e8695d9d05ef41d650dd19d
Salomon <salomond.c@hotmail.com>
2024-11-19 03:29:13 -0300

    fix slug chat layout

--- src/app/chat/[slug]/page.tsx
+++ src/app/chat/[slug]/page.tsx
@@ -1,12 +1,37 @@
-import { ChatMessages } from "@/components";
+"use client";
+import {
+	ChatContainer,
+	ChatHistoryContainer,
+	ChatInput,
+	ChatMessages,
+	ChatUserHeader,
+	ChatVersion,
+	Icons,
+} from "@/components";
+import { useContent } from "@/context/Content";
 
-export default async function SavedChat({
-	params,
-}: {
-	params: Promise<{ slug: string }>;
-}) {
-	const slug = (await params).slug;
-	console.log(slug);
-
-	return <ChatMessages />;
+export default function SavedChat() {
+	const { isLoading } = useContent();
+	return (
+		<>
+			{isLoading ? (
+				<div className="h-screen w-screen flex items-center justify-center gap-2 flex-col">
+					<Icons.Loading />
+				</div>
+			) : (
+				<>
+					<ChatHistoryContainer />
+					<div className="flex flex-grow">
+						<ChatContainer>
+							<ChatUserHeader />
+							<ChatMessages />
+							<div className="border-t md:hidden" />
+							<ChatInput />
+							<ChatVersion />
+						</ChatContainer>
+					</div>
+				</>
+			)}
+		</>
+	);
 }
