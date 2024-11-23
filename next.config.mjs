4faecbc54e2952efcc509c991982ccaff363b541
Salomon <salomond.c@hotmail.com>
2024-11-23 07:25:10 -0300

    Set API url as env var

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "https://make.lumnar.tech/:path*",
-			},
-		];
+		return process.env.NODE_ENV === "development"
+			? [
+					{
+						source: "/api/:path*",
+						destination: "https://make.lumnar.tech/:path*",
+					},
+			  ]
+			: [];
 	},
 };
 
