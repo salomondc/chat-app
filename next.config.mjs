f5b75616109a75852a8d7e5035a90b49f40ae820
Salomon <salomond.c@hotmail.com>
2024-11-23 18:08:19 -0300

    Fix missing return on rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	async rewrites() {
-		[
+		return [
 			{
 				source: "/api/:path*",
 				destination: "https://make.lumnar.tech/:path*",
