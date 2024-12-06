f357ad2ce3652d3c31e0612e45b16d41d1717d5e
Salomon <salomond.c@hotmail.com>
2024-12-06 01:04:35 -0300

    Test cors

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,21 @@ const nextConfig = {
 			},
 		];
 	},
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{ key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
